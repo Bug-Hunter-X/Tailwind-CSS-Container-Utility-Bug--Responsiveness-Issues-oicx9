6528eed1f60ad2627ba4d9aa9a6d692d2b33a085
BugHunter-X <askzenai@gmail.com>
2024-12-17 20:16:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,11 @@
+```javascript
+// Example using Tailwind's `container` utility
+<div class="container mx-auto">
+  <h1>This is a container</h1>
+</div>
+
+// Another example, applying multiple classes
+<div class="container mx-auto p-4 bg-gray-200 rounded">
+  <p>This is another container with additional styling.</p>
+</div>
+```
